b7c94702f6b72c988047ddbc363f276a40436ddd
Ansh Sharma <sharmaansh6717@gmail.com>
2026-08-31 17:59:40 +0000

    book done

---
+++ models/Book.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const bookSchema = new mongoose.Schema(
+    {
+        title : {
+            type : String,
+            required : true
+        },
+        author : {
+            type : String,
+            required : true 
+        },
+        category : {
+            type : String
+        },
+        available : {
+            type : String
+        },
+        issuedTo : {
+            type: String,
+            default: null
+        }
+    });
+    module.exports = mongoose.model('Books' , bookSchema);
