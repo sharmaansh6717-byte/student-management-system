d310e7ca3103cb42d914777db32103face8003a5
Ansh Sharma <sharmaansh6717@gmail.com>
2026-09-01 07:01:40 +0000

    Hashing of the password done

--- models/User.js
+++ models/User.js
@@ -21,3 +21,17 @@ const userSchema = new mongoose.Schema({
         default : 'user'
     }
 })
+userSchema.pre('save', async function (next) {
+  // Only hash the password if it's new or being changed
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// A helper method to check login password against the stored hash
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
